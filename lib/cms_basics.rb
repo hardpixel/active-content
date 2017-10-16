7301079307a02c638b70f1a86a03b04c7b0af85a
Olibia Tsati <olibia@hardpixel.eu>
2017-10-16 16:28:40 +0300

    add gem dependencies

--- lib/cms_basics.rb
+++ lib/cms_basics.rb
@@ -1,4 +1,15 @@
+require "ancestry"
 require "cms_basics/version"
+require_relative "cms_basics/categorizable"
+require_relative "cms_basics/taggable"
+require_relative "cms_basics/templatable"
+require_relative "cms_basics/post"
+require_relative "cms_basics/page"
+require_relative "cms_basics/models/template"
+require_relative "cms_basics/models/categorization"
+require_relative "cms_basics/models/tagging"
+require_relative "cms_basics/models/post_tag"
+require_relative "cms_basics/models/post_category"
 
 module CmsBasics
   # Your code goes here...
