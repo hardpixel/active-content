29168e135a0ac0d85787a320f208acf40273d500
Olibia Tsati <olibia@hardpixel.eu>
2017-11-08 12:18:40 +0200

    optimize install generator

--- lib/generators/cms_basics/install_generator.rb
+++ lib/generators/cms_basics/install_generator.rb
@@ -6,37 +6,18 @@ module CmsBasics
   class InstallGenerator < Rails::Generators::Base
     include Rails::Generators::Migration
 
+    # Model names
+    MODEL_NAMES  = ['page', 'post', 'category', 'tag', 'template'].freeze
+    MIGRATE_ONLY = ['template'].freeze
+
     desc 'Generates migrations to add CMS tables.'
     source_root File.expand_path('../templates', __FILE__)
-    class_option :page, type: :boolean, default: true, desc: 'Add page table.'
-    class_option :post, type: :boolean, default: true, desc: 'Add post table.'
-    class_option :category, type: :boolean, default: true, desc: 'Add category table.'
-    class_option :tag, type: :boolean, default: true, desc: 'Add tag table.'
-    class_option :template, type: :boolean, default: true, desc: 'Add template table.'
+    argument :models, type: :array, default: MODEL_NAMES, enum: MODEL_NAMES, desc: 'Add CMS models tables.'
 
     def create_migration_file
-      if options.page?
-        template 'model/page.rb', 'app/models/page.rb'
-        migration_template 'migration/page.rb', 'db/migrate/create_pages.rb'
-      end
-
-      if options.post?
-        template 'model/post.rb', 'app/models/post.rb'
-        migration_template 'migration/post.rb', 'db/migrate/create_posts.rb'
-      end
-
-      if options.category?
-        template 'model/category.rb', 'app/models/category.rb'
-        migration_template 'migration/category.rb', 'db/migrate/create_categories.rb'
-      end
-
-      if options.tag?
-        template 'model/tag.rb', 'app/models/tag.rb'
-        migration_template 'migration/tag.rb', 'db/migrate/create_tags.rb'
-      end
-
-      if options.template?
-        migration_template 'migration/template.rb', 'db/migrate/create_templates.rb'
+      models.each do |model|
+        template "model/#{model}.rb", "app/models/#{model}.rb" unless model.in? MIGRATE_ONLY
+        migration_template "migration/#{model}.rb", "db/migrate/create_#{model.pluralize}.rb"
       end
     end
 
