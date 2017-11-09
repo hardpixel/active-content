6e64e6aa68d9757b8a3b5a06716f1634bef8e78e
Jonian Guveli <jonian@hardpixel.eu>
2017-11-09 23:12:38 +0200

    update attachable concern

--- lib/cms_basics/attachable.rb
+++ lib/cms_basics/attachable.rb
@@ -9,10 +9,18 @@ module Cms
 
         if options[:multiple]
           has_many :"#{name}_attachments", assoc_proc, assoc_opts
-          has_many :"#{name}_media", through: :"#{name}_attachments", source: :medium
+          has_many :"#{name}", through: :"#{name}_attachments", source: :medium
         else
           has_one :"#{name}_attachment", assoc_proc, assoc_opts
-          has_one :"#{name}_medium", through: :"#{name}_attachment", source: :medium
+          has_one :"#{name}", through: :"#{name}_attachment", source: :medium
+
+          define_method :"#{name}_id" do
+            send(:"#{name}").try :id
+          end
+
+          define_method :"#{name}_id=" do |value|
+            send :"#{name}=", Medium.find(value)
+          end
         end
       end
     end
