ba45ba26229fb8617eb2590b3f3611863083cec3
Jonian Guveli <jonian@hardpixel.eu>
2017-11-09 12:51:31 +0200

    double2single quotes

--- lib/cms_basics.rb
+++ lib/cms_basics.rb
@@ -1,18 +1,18 @@
-require "active_support"
-require "ancestry"
-require "enumerize"
-require "carrierwave"
-require "cms_basics/categorizable"
-require "cms_basics/taggable"
-require "cms_basics/templatable"
-require "cms_basics/attachable"
-require "cms_basics/models/template"
-require "cms_basics/models/categorization"
-require "cms_basics/models/tagging"
-require "cms_basics/models/post_tag"
-require "cms_basics/models/post_category"
-require "cms_basics/models/attachment"
-require "cms_basics/version"
+require 'active_support'
+require 'ancestry'
+require 'enumerize'
+require 'carrierwave'
+require 'cms_basics/categorizable'
+require 'cms_basics/taggable'
+require 'cms_basics/templatable'
+require 'cms_basics/attachable'
+require 'cms_basics/models/template'
+require 'cms_basics/models/categorization'
+require 'cms_basics/models/tagging'
+require 'cms_basics/models/post_tag'
+require 'cms_basics/models/post_category'
+require 'cms_basics/models/attachment'
+require 'cms_basics/version'
 
 module CmsBasics
   # Your code goes here...
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,4 @@
-$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
-require "cms_basics"
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
-require "minitest/autorun"
+require 'cms_basics'
+require 'minitest/autorun'
