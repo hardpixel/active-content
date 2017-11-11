66faa9c0605ea9e2cd3c738cf3f6c326bc4b8981
Jonian Guveli <jonian@hardpixel.eu>
2017-11-11 13:43:41 +0200

    optimize templatable concern

--- lib/cms_basics/templatable.rb
+++ lib/cms_basics/templatable.rb
@@ -3,41 +3,32 @@ module CmsBasics
     extend ActiveSupport::Concern
 
     class_methods do
-      def has_templates(*names)
+      def has_templates(*templates)
         extend Enumerize
         include ActiveDelegate
 
-        enumerize :template, in: names, predicates: { prefix: true }
+        enumerize :template, in: templates, predicates: { prefix: true }
 
         has_one :view_template, as: :templatable, class_name: 'Template', autosave: true, dependent: :destroy
         delegate_attributes :template, to: :view_template
 
-        _define_view_templates_callbacks
-        _define_view_templates_scopes
+        before_save do
+          self.view_template = nil if template.nil?
+        end
+
+        query = self.includes(:view_template)
+        blank = { templates: { template: nil } }
+
+        scope :with_template, -> (*names) do
+          named = { templates: { template: names } }
+          names.empty? ? query.where.not(blank) : query.where(named)
+        end
+
+        scope :without_template, -> (*names) do
+          named = { templates: { template: names } }
+          names.empty? ? query.where(blank) : query.where(blank).or(query.where.not(named))
+        end
       end
-
-      private
-
-        def _define_view_templates_callbacks
-          before_save do
-            self.view_template = nil if template.nil?
-          end
-        end
-
-        def _define_view_templates_scopes
-          query = self.includes(:view_template)
-          blank = { templates: { template: nil } }
-
-          scope :with_template, -> (*names) do
-            named = { templates: { template: names } }
-            names.empty? ? query.where.not(blank) : query.where(named)
-          end
-
-          scope :without_template, -> (*names) do
-            named = { templates: { template: names } }
-            names.empty? ? query.where(blank) : query.where(blank).or(query.where.not(named))
-          end
-        end
     end
   end
 end
