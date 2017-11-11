dfc7e90129511f7c851ef6177e213ec372cc6d9c
Jonian Guveli <jonian@hardpixel.eu>
2017-11-11 13:40:35 +0200

    optimize uploadable

--- lib/cms_basics/uploadable.rb
+++ lib/cms_basics/uploadable.rb
@@ -6,27 +6,18 @@ module CmsBasics
       def has_uploader(name=nil)
         mount_uploader :file, name
 
-        _define_media_record_attributes
-        _define_media_uploader_callbacks
+        define_method :mime_type do
+          @mime_type ||= MIME::Types[file.content_type].first
+        end
+
+        before_save do
+          if file_identifier.present?
+            self.name       = file_identifier.split('.').first.titleize unless name?
+            self.media_type = mime_type.media_type
+            self.sub_type   = mime_type.sub_type
+          end
+        end
       end
-
-      private
-
-        def _define_media_record_attributes
-          define_method :mime_type do
-            @mime_type ||= MIME::Types[file.content_type].first
-          end
-        end
-
-        def _define_media_uploader_callbacks
-          before_save do
-            if file_identifier.present?
-              self.name       = file_identifier.split('.').first.titleize unless name?
-              self.media_type = mime_type.media_type
-              self.sub_type   = mime_type.sub_type
-            end
-          end
-        end
     end
   end
 end
