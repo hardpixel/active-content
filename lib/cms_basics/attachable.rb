1364efe8da0c5ae280581ff7ddd17b73887f22fa
Jonian Guveli <jonian@hardpixel.eu>
2017-11-11 12:35:25 +0200

    optimize attachable

--- lib/cms_basics/attachable.rb
+++ lib/cms_basics/attachable.rb
@@ -7,12 +7,15 @@ module CmsBasics
         assoc_opts = { as: :attachable, class_name: 'Attachment', autosave: true, dependent: :destroy }
         assoc_proc = -> { where name: name }
 
-        if options[:multiple]
+        multiple = options.delete(:multiple)
+        options  = options.reverse_merge(through: :"#{name}_attachments", source: :upload)
+
+        if multiple
           has_many :"#{name}_attachments", assoc_proc, assoc_opts
-          has_many :"#{name}", through: :"#{name}_attachments", source: :upload
+          has_many :"#{name}", options
         else
           has_one :"#{name}_attachment", assoc_proc, assoc_opts
-          has_one :"#{name}", through: :"#{name}_attachment", source: :upload
+          has_one :"#{name}", options
 
           define_method :"#{name}_id" do
             send(:"#{name}").try :id
