aebc3edf540495c06b80ce0f98ff3960eaab3221
Jonian Guveli <jonian@hardpixel.eu>
2017-11-11 12:25:46 +0200

    fix taxonomizable concern

--- lib/cms_basics/taxonomizable.rb
+++ lib/cms_basics/taxonomizable.rb
@@ -5,26 +5,24 @@ module CmsBasics
     class_methods do
       def has_taxonomy(name, options={})
         options = options.reverse_merge(class_name: name.to_s.classify)
-        options = options.merge(through: :taxonomizations, source: :taxonomy)
+        options = options.merge(through: :"#{name}_taxonomizations", source: :taxonomy)
+        setting = { as: :taxonomizable, class_name: 'Taxonomization', autosave: true, dependent: :destroy }
 
-        has_many :taxonomizations, as: :taxonomizable
+        has_many :"#{name}_taxonomizations", setting
         has_many name, options
 
-        scope :taxonomized,   -> { joins(name).distinct }
-        scope :with_taxonomy, -> ids { taxonomized.where name => { id: ids } }
-
-        _define_taxonomy_list_methods(name)
+        _define_taxonomy_list_methods(name, options)
       end
 
       private
 
-        def _define_taxonomy_list_methods(name)
+        def _define_taxonomy_list_methods(name, options)
           define_method :"#{name}_list" do
             self.send name
           end
 
           define_method :"#{name}_list=" do |names|
-            model = name.to_s.classify.constantize
+            model = options[:class_name].classify.constantize
             names = Array(names).map do |n|
               model.find_by_name(n) || model.new(name: n)
             end
