283089f66c8d9d7ee936c15f984205d7d3a68871
Jonian Guveli <jonian@hardpixel.eu>
2017-11-12 15:28:28 +0200

    use delegate_attribute in metable concern

--- lib/cms_basics/metable.rb
+++ lib/cms_basics/metable.rb
@@ -8,12 +8,10 @@ module CmsBasics
 
         assoc_opts = { as: :metable, class_name: 'Metum', autosave: true, dependent: :destroy }
         assoc_proc = -> { where field: field }
+        value_opts = { to: :"#{field}_metum", prefix: field, alias: field, default: options[:default] }
 
         has_one :"#{field}_metum", assoc_proc, assoc_opts
-        delegate_attributes only: [:value], to: :"#{field}_metum", prefix: field
-
-        attribute :"#{field}", cast_type, default: options[:default]
-        alias_attribute :"#{field}", :"#{field}_value"
+        delegate_attribute :value, cast_type, value_opts
       end
     end
   end
