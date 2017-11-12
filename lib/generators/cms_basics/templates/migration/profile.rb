5d2a6b6790d2318639f181f8b4f96a123d0a83b1
Olibia Tsati <olibia@hardpixel.eu>
2017-11-12 18:38:12 +0200

    remove type from profiles

--- lib/generators/cms_basics/templates/migration/profile.rb
+++ lib/generators/cms_basics/templates/migration/profile.rb
@@ -1,7 +1,6 @@
 class CreateProfiles < ActiveRecord::Migration[5.0]
   def change
     create_table :profiles do |t|
-      t.string     :type
       t.string     :first_name
       t.string     :last_name
       t.text       :bio
