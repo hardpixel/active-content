f952a2d47806bdf8ecdf6c3aa2bfe414e8ee3223
Jonian Guveli <jonian@hardpixel.eu>
2017-11-12 21:45:09 +0200

    update upload model

--- lib/active_content/models/upload.rb
+++ lib/active_content/models/upload.rb
@@ -1,6 +1,10 @@
 class ActiveContent::Upload < ApplicationRecord
+  # Set table name
   self.table_name = 'uploads'
 
-  # Mount carrierwave uploader
+  # Mount media uploader
   has_uploader
+
+  # Define validations
+  validates :file, presence: true
 end
