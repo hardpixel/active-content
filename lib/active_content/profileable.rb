24c62f2705c4237843fcd1b8a17d8092ea778d6f
Jonian Guveli <jonian@hardpixel.eu>
2017-11-13 11:49:54 +0200

    avoid creating empty profiles

--- lib/active_content/profileable.rb
+++ lib/active_content/profileable.rb
@@ -9,6 +9,17 @@ module ActiveContent
         has_one :profile, as: :profileable, class_name: 'ActiveContent::Profile', autosave: true, dependent: :destroy
         delegate_attributes to: :profile, allow_nil: true
 
+        before_save do
+          prof_foreign = self.class.profiles_attribute_names.map(&:to_s)
+          prof_current = prof_foreign.map { |n| send(n) }
+          prof_default = self.class.column_defaults.select { |n, _d| prof_foreign.include?(n) }.values
+          prof_changes = (prof_current - prof_default).reject(&:blank?)
+
+          if prof_changes.empty?
+            self.profile = nil
+          end
+        end
+
         define_method :name do
           "#{first_name} #{last_name}".strip
         end
