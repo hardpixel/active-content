149d211ac4d1ef588541c28457248b4e3f7d254b
Jonian Guveli <jonian@hardpixel.eu>
2017-11-13 11:31:29 +0200

    add content and taxonomy validations

--- lib/active_content/models/content.rb
+++ lib/active_content/models/content.rb
@@ -1,3 +1,7 @@
 class ActiveContent::Content < ApplicationRecord
+  # Set table name
   self.table_name = 'contents'
+
+  # Define validations
+  validates :title, presence: true
 end
--- lib/active_content/models/taxonomy.rb
+++ lib/active_content/models/taxonomy.rb
@@ -1,3 +1,7 @@
 class ActiveContent::Taxonomy < ApplicationRecord
+  # Set table name
   self.table_name = 'taxonomies'
+
+  # Define validations
+  validates :name, presence: true
 end
