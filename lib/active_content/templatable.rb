e53571dd8cb82024a13a2e58805a575ef356f21f
Jonian Guveli <jonian@hardpixel.eu>
2017-11-15 16:28:25 +0200

    use active_delegate scopes and finders in templatable

--- lib/active_content/templatable.rb
+++ lib/active_content/templatable.rb
@@ -10,23 +10,10 @@ module ActiveContent
         enumerize :template, in: templates, predicates: { prefix: true }
 
         has_one :view_template, as: :templatable, class_name: 'ActiveContent::Template', autosave: true, dependent: :destroy
-        delegate_attribute :name, :string, to: :view_template, prefix: 'template', alias: :template
+        delegate_attribute :name, :string, to: :view_template, prefix: 'template', alias: :template, finder: true, scope: true
 
         before_save do
           self.view_template = nil if template.blank?
-        end
-
-        query = self.includes(:view_template)
-        blank = { templates: { name: nil } }
-
-        scope :with_template, -> (*names) do
-          named = { templates: { name: names } }
-          names.empty? ? query.where.not(blank) : query.where(named)
-        end
-
-        scope :without_template, -> (*names) do
-          named = { templates: { name: names } }
-          names.empty? ? query.where(blank) : query.where(blank).or(query.where.not(named))
         end
       end
     end
