3305e25ac620a0674ce9d5ff4fd3611a8df6337b
Jonian Guveli <jonian@hardpixel.eu>
2017-11-15 16:27:35 +0200

    add finder and scope support in metable
    passes options to active_delegate

--- lib/active_content/metable.rb
+++ lib/active_content/metable.rb
@@ -8,7 +8,7 @@ module ActiveContent
 
         assoc_opts = { as: :metable, class_name: 'ActiveContent::Metum', autosave: true, dependent: :destroy }
         assoc_proc = -> { where field: field }
-        value_opts = { to: :"#{field}_metum", prefix: field, alias: field, default: options[:default] }
+        value_opts = options.merge(to: :"#{field}_metum", prefix: field, alias: field)
 
         has_one :"#{field}_metum", assoc_proc, assoc_opts
         delegate_attribute :value, cast_type, value_opts
