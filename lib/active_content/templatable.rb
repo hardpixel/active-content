e0c86676780b67eadaf8c389287460f9f5903bf5
Jonian Guveli <jonian@hardpixel.eu>
2017-12-11 12:53:21 +0200

    update templatable for active_delegate

--- lib/active_content/templatable.rb
+++ lib/active_content/templatable.rb
@@ -10,7 +10,7 @@ module ActiveContent
         enumerize :template, in: templates, predicates: { prefix: true }
 
         has_one :view_template, as: :templatable, class_name: 'ActiveContent::Template', autosave: true, dependent: :destroy
-        delegate_attribute :name, :string, to: :view_template, prefix: 'template', alias: :template, finder: true, scope: true
+        delegate_attribute :name, :enumerize, to: :view_template, prefix: 'template', alias: :template, finder: true, scope: true, cast: false
 
         before_save do
           self.view_template = nil if template.blank?
