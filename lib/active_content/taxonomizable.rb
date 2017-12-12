fba2d141c5e7444f11368e92d3a90a493785dd60
Jonian Guveli <jonian@hardpixel.eu>
2017-12-12 16:01:57 +0200

    fix taxonomies _list methods

--- lib/active_content/taxonomizable.rb
+++ lib/active_content/taxonomizable.rb
@@ -12,16 +12,16 @@ module ActiveContent
         has_many name, options
 
         define_method :"#{name}_list" do
-          self.send name
+          self.send(name).pluck(:name)
         end
 
-        define_method :"#{name}_list=" do |names|
+        define_method :"#{name}_list=" do |*names|
           model = options[:class_name].classify.constantize
-          names = Array(names).map do |n|
+          items = names.flatten.reject(&:blank?).map do |n|
             model.find_by_name(n) || model.new(name: n)
           end
 
-          self.send :"#{name}=", names
+          self.send :"#{name}=", items
         end
 
         scope :"with_#{name}", -> (*ids) do
