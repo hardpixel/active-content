4c4e05c6d2704198112e72beda57a509f332f9e8
Jonian Guveli <jonian@hardpixel.eu>
2017-12-18 15:30:43 +0200

    add suport for default in templates

--- lib/active_content/templatable.rb
+++ lib/active_content/templatable.rb
@@ -3,11 +3,15 @@ module ActiveContent
     extend ActiveSupport::Concern
 
     class_methods do
-      def has_templates(*templates)
+      def has_templates(*args)
         extend Enumerize
         include ActiveDelegate
 
-        enumerize :template, in: templates, predicates: { prefix: true }
+        options   = args.extract_options!
+        templates = args
+        default   = options.fetch, :default, templates.first
+
+        enumerize :template, in: templates, predicates: { prefix: true }, default: default
 
         has_one :view_template, as: :templatable, class_name: 'ActiveContent::Template', autosave: true, dependent: :destroy
         delegate_attribute :name, :string, to: :view_template, prefix: 'template', alias: :template, finder: true, scope: true
