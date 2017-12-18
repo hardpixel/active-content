65c2edeb7059a96be80e39daa6928213f0c863f2
Olibia Tsati <olibia@hardpixel.eu>
2017-12-18 21:50:33 +0200

    do not save empty meta

--- lib/active_content/metable.rb
+++ lib/active_content/metable.rb
@@ -12,6 +12,10 @@ module ActiveContent
 
         has_one :"#{field}_metum", assoc_proc, assoc_opts
         delegate_attribute :value, cast_type, value_opts
+
+        before_save do
+          self.send(:"#{field}_metum=", nil) if send(field).to_s.blank?
+        end
       end
     end
   end
