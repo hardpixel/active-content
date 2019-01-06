8c4a17b2d8a7ed795f16b7be31047491ff20f8d1
Jonian Guveli <jonian@hardpixel.eu>
2019-01-06 13:01:52 +0200

    change private block indentation

--- lib/generators/active_content/model_generator.rb
+++ lib/generators/active_content/model_generator.rb
@@ -15,32 +15,32 @@ module ActiveContent
 
     private
 
-      def model_class
-        name.camelize
+    def model_class
+      name.camelize
+    end
+
+    def type_class
+      type.camelize
+    end
+
+    def model_file
+      "#{name.downcase.underscore}"
+    end
+
+    def model_meta
+      args.map do |arg|
+        field, cast_type, default = arg.split(':')
+
+        items  = [":#{field.to_sym}", ":#{cast_type.to_sym}"]
+        items << ["default: #{value_type_cast(cast_type.to_sym, default)}"] if default.present?
+
+        items.join(', ')
       end
+    end
 
-      def type_class
-        type.camelize
-      end
-
-      def model_file
-        "#{name.downcase.underscore}"
-      end
-
-      def model_meta
-        args.map do |arg|
-          field, cast_type, default = arg.split(':')
-
-          items  = [":#{field.to_sym}", ":#{cast_type.to_sym}"]
-          items << ["default: #{value_type_cast(cast_type.to_sym, default)}"] if default.present?
-
-          items.join(', ')
-        end
-      end
-
-      def value_type_cast(cast_type, value)
-        strings = [:string, :date, :datetime, :time]
-        cast_type.in?(strings) ? "'#{value}'" : value
-      end
+    def value_type_cast(cast_type, value)
+      strings = [:string, :date, :datetime, :time]
+      cast_type.in?(strings) ? "'#{value}'" : value
+    end
   end
 end
